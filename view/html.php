07be1ef16d76a7949cfbfc69558da48fb75d89d2
TanPham061293 <134413763+TanPham061293@users.noreply.github.com>
2023-06-30 12:56:28 +0700

    update

--- view/html.php
+++ view/html.php
@@ -4,7 +4,7 @@
 <meta charset="${encoding}">
 <link  href ="view/css/format.css" rel ="stylesheet">
 
-<title>Insert title here</title>
+<title>FE-BE</title>
 </head>
 <body>
 <?php 
@@ -23,9 +23,9 @@ if (isset($this->post)){
 
 ?>
     <div id ="warper">
-    	<div class ="img"><img class ="img1" alt="chua tai" src="view/img/duytan.jpg">
-    	<p><i>Hello!</i><br>Pham Duy Tan</p>
-    					  <img class ="img2" alt="chua tai" src="view/img/ZenS.png"></div>	
+    	<div class ="img"><img class ="img1" alt="chua tai" src="view/img/Joke2.png">
+    	<p><i>Handicrafted by</i><br><span class = "bolder">Jim HLS</span></p>
+    					  <img class ="img2" alt="chua tai" src="view/img/Joke1.png"></div>	
     <div id ="header" ><p class ="main">A joke a day keeps the doctor away</p>
     			       <p class = "other"> If you joke wrong way, your teeth have to pay. (Serious)</p>
         </div>
@@ -43,7 +43,7 @@ if (isset($this->post)){
         <div id ="footer"><p>This website is created as part of Hisolutions program.  The materials contained on this website are provided for general<br> 
                                 information only and do not constitute any form of advice.  HLS assumes no responsibility for the accuracy of any particular statement and <br>
                                 accepts no liability for any loss or damage which may arise from reliance on the information contained on this site.<br></p>
-                                <p>Copyright 2021 HLS</p>
+                                <p class = "bolder">Copyright 2021 HLS</p>
           </div>
     </div>
 	
