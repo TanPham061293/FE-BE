bb4ceee24d4f179360704b1a8ee958d43dbd1285
TanPham061293 <134413763+TanPham061293@users.noreply.github.com>
2023-07-02 11:03:34 +0700

    update css

--- view/html.php
+++ view/html.php
@@ -2,8 +2,8 @@
 <html>
 <head>
 <meta charset="${encoding}">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <link  href ="view/css/format.css" rel ="stylesheet">
-
 <title>FE-BE</title>
 </head>
 <body>
@@ -22,7 +22,7 @@ if (isset($this->post)){
 }
 
 ?>
-    <div id ="warper">
+    <div id ="warper" >
     	<div class ="img"><img class ="img1" alt="chua tai" src="view/img/Joke2.png">
     	<p><i>Handicrafted by</i><br><span class = "bolder">Jim HLS</span></p>
     					  <img class ="img2" alt="chua tai" src="view/img/Joke1.png"></div>	
@@ -40,7 +40,7 @@ if (isset($this->post)){
                     </div>'
                 ?>	
         </div>
-        <div id ="footer"><p>This website is created as part of Hisolutions program.  The materials contained on this website are provided for general<br> 
+        <div id ="footer" ><p>This website is created as part of Hisolutions program.  The materials contained on this website are provided for general<br> 
                                 information only and do not constitute any form of advice.  HLS assumes no responsibility for the accuracy of any particular statement and <br>
                                 accepts no liability for any loss or damage which may arise from reliance on the information contained on this site.<br></p>
                                 <p class = "bolder">Copyright 2021 HLS</p>
